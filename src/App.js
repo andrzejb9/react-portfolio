19161b1a9745d26184375890c94f374f21949ffc
andrzejb9 <a.bankowski9@o2.pl>
2023-06-24 01:56:22 +0200

    page transition

--- src/App.js
+++ src/App.js
@@ -5,19 +5,27 @@ import AboutUs from "./pages/AboutUs";
 import ContactUs from "./pages/ContactUs";
 import OurWork from "./pages/OurWork";
 import Nav from "./components/Nav";
+import MovieDetail from "./pages/MovieDetail";
 //Router
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
+//Animation
+import { AnimatePresence } from "framer-motion";
 
 function App() {
+  const location = useLocation();
+
   return (
     <div className="App">
       <GlobalStyle />
       <Nav />
-      <Routes>
-        <Route path="/" exact element={<AboutUs />} />
-        <Route path="/work" element={<OurWork />} />
-        <Route path="/contact" element={<ContactUs />} />
-      </Routes>
+      <AnimatePresence mode="wait">
+        <Routes location={location} key={location.pathname}>
+          <Route path="/" exact element={<AboutUs />} />
+          <Route path="/work" exact element={<OurWork />} />
+          <Route path="/work/:id" element={<MovieDetail />} />
+          <Route path="/contact" element={<ContactUs />} />
+        </Routes>
+      </AnimatePresence>
     </div>
   );
 }
--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,14 +1,23 @@
+//Page Components
 import AboutSection from "../components/AboutSection";
 import ServicesSection from "../components/ServicesSection";
 import FaqSection from "../components/FaqSection";
+//Animations
+import { motion } from "framer-motion";
+import { pageAnimation } from "../animation";
 
 function AboutUs() {
   return (
-    <>
+    <motion.div
+      exit="exit"
+      variants={pageAnimation}
+      initial="hidden"
+      animate="show"
+    >
       <AboutSection />
       <ServicesSection />
       <FaqSection />
-    </>
+    </motion.div>
   );
 }
 
