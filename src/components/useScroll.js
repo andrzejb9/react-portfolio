5dfe15a0433510ffaa51b30b7ca8064b75c6e22a
andrzejb9 <a.bankowski9@o2.pl>
2023-06-26 00:21:50 +0200

    scroll animation

---
+++ src/components/useScroll.js
@@ -0,0 +1,18 @@
+import { useEffect } from "react";
+import { useInView } from "react-intersection-observer";
+import { useAnimationControls } from "framer-motion";
+
+export const useScroll = () => {
+  const controls = useAnimationControls();
+  const [element, view] = useInView({ threshold: 0.5 });
+
+  useEffect(() => {
+    if (view) {
+      controls.start("show");
+    } else {
+      controls.start("hidden");
+    }
+  }, [element, view]);
+
+  return [element, controls];
+};
